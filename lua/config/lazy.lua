966c13cf828c28ac296cd4a333f916aa95211bd3
CarlHermany <152496948+CarlHermany@users.noreply.github.com>
2024-02-29 18:24:53 +0100

    first commit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,9 +24,10 @@ require("lazy").setup({
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
+    colorscheme = "peachpuff",
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "peachpuff", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
